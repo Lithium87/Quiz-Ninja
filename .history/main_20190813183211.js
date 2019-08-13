41af1a0f5f24c40adde0a45f9b5fcbb74d34af4e
Lithium87 <cibs@abv.bg>
2019-08-13 18:34:13 +0300

    Improved with namespace

--- .history/main_20190813183211.js
+++ .history/main_20190813183211.js
@@ -37,3 +37,37 @@ const game = {
 }
 
 game.start(quiz);
+
+/*
+function start(quiz) {
+    let score = 0;
+
+    // main game loop
+    for (const [question, answer] of quiz) {
+        const response = ask(question);
+        check(response, answer);
+    }
+    // end of main game loop
+
+    gameOver();
+
+    function ask(question) {
+        return prompt(question);
+    }
+
+    function check(response, answer) {
+        if (response === answer) {
+            alert("Correct!");
+            score++;
+        } else {
+            alert(`Wrong! The correct answer was ${answer}`);
+        }
+    }
+
+    function gameOver() {
+        alert(`Game Over! Your score is: ${score} point${score !== 1 ? 's' : ''}`);
+    }
+}
+
+start(quiz);
+*/
